aac252073fc1d8b03c50f707868c2aba434b4dd9
PytTech <126572994+PytTech@users.noreply.github.com>
2026-08-07 11:06:41 +0530

    Add SQL queries for customers and orders
    Added SQL queries to list customer names, order dates, and product names.

---
+++ Experiment4/4.1/code.sql
@@ -0,0 +1,8 @@
+-- Customers and Orders: List the customer_name and order_date for all customers who have placed orders.
+-- All Customers and Their Orders: List all customer names and their corresponding product_name from orders, if they have any. Include customers even if they haven't placed any orders.
+-- Find Products and Their Orders: Display Product Name and the order_date from all the products that are ordered.
+
+select c.customer_name, o.order_date from customers c join orders o on c.customer_id=o.customer_id;
+select c.customer_name, o.product_name from customers c left join orders o on c.customer_id=o.customer_id;
+select p.product_name, o.order_date from products p join orders o on p.product_name=o.product_name;
+
